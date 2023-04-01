65d6ab5a5fc7804f9b2bffdc1b42f0785bb172ed
Cổ Hoặc Kim <CoHoacKim@Hoangs-MacBook-Air.local>
2023-04-01 21:47:03 +0700

    first commit

---
+++ BaiTapVeNha/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace BaiTapVeNha.Models{
+	public class Order{
+		public Order(){
+
+        }
+        public int Id { get; set; }
+        public DateTime OrderPhanced { get; set; }
+        public DateTime? OrderFulfilled { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customers { get; set; } = null!;
+        public ICollection<OrderDetail> OrderDetail { get; set; } = null!;
+    }
+}
+
---
+++ BaiTapVeNha/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BaiTapVeNha.Models{
+	public class Product{
+		public Product(){
+
+		}
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        [Column(TypeName = "decimal(6, 2)")]
+        public decimal Price { get; set; }
+    }
+}
+
